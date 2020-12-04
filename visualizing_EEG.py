67a524abcd7635a5c9f7471a81383e91bfa6db99
Jiseungwoo <67452897+viriditass@users.noreply.github.com>
2020-12-04 14:20:18 +0900

    fix

--- visualizing_EEG.py
+++ visualizing_EEG.py
@@ -1,9 +1,32 @@
+"""import mne
+
+raw = mne.io.read_raw_edf("C:/Users/user/Desktop/AI/EEG_classification-main/EEG/data/SC4001E0-PSG.edf", preload=True)
+mne.set_log_level("WARNING")
+print("raw: ")
+print(raw)
+print("raw info: ")
+print(raw.info)
+print("rwa info [sfreq]")
+print(raw.info["sfreq"])
+
+raw.rename_channels(lambda s: s.strip("."))
+print(mne.channels.get_builtin_montages())
+mne.channels.get_builtin_montages()
+montage = mne.channels.make_standard_montage("EGI_256") # "standard_1020"
+montage.plot()
+#raw.plot()"""
+
 import mne
 
+
 mne.set_log_level("WARNING")
-raw = mne.io.read_raw_edf("S001R04.edf", preload=True)
+raw = mne.io.read_raw_edf("C:/Users/user/Desktop/AI/EEG_classification-main/EEG/data/SC4001E0-PSG.edf", preload=True)
+
 raw.rename_channels(lambda s: s.strip("."))
-raw.set_montage("standard_1020", match_case=False)
-raw.set_eeg_reference("average")
+# print(mne.channels.get_builtin_montages())
+
+
+mne.channels.make_standard_montage("EGI_256")
+#raw.set_eeg_reference("average")
 
 raw.plot()
